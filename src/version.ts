5acb58f47962f73a2c828e386f17879a131e6541
tettuan <tettuan@gmail.com>
2025-07-07 22:47:03 +0900

    chore: bump version to 0.0.1 for initial JSR release

--- src/version.ts
+++ src/version.ts
@@ -5,7 +5,7 @@
  * The current version of tmux-monitor, synchronized with deno.json.
  * @module
  */
-export const VERSION = "1.0.0";
+export const VERSION = "0.0.1";
 
 /**
  * Get version information
